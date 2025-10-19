1b452d537ded8382a1d806a559bca3c69c26054d
Hansem Ro <hansemro@outlook.com>
2025-10-19 11:11:06 +0200

    Add firmware to enable FPGA

---
+++ firmware/enable_fpga/main.c
@@ -0,0 +1,36 @@
+// Copyright (C) Hansem Ro <hansemro@outlook.com>
+// SPDX-License-Identifier: MIT
+
+#include <fx2regs.h>
+
+void init(void) {
+    // Select Port Function
+    PORTACFG = 0;
+    PORTCCFG = 0;
+    PORTECFG = 0;
+    // Use PB and PD port function
+    IFCONFIG = _IFCLKSRC | _3048MHZ;
+
+    // Set external JTAG TCK buffer to tristate by setting CTL2 low
+    GPIFCTLCFG = 0;
+    GPIFIDLECTL &= ~(_CTL2);
+    GPIFABORT = 0xff; // Enter IDLE state
+
+    // Tristate pins to FPGA
+    OEA = 0x00;
+    OEB = 0x00;
+    OEC = 0x00;
+    OED = 0x00;
+    OEE = 1U << 6;
+
+    // Enable FPGA Power
+    IOE = 1U << 6;
+
+    // Enable IFCLK
+    IFCONFIG |= _IFCLKOE;
+}
+
+int main(void) {
+    init();
+    while(1);
+}
