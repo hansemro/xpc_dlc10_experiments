0a3d52b2d51fc82c516ecbdec0ae4d0d98beafd0
Hansem Ro <hansemro@outlook.com>
2025-10-21 01:50:13 +0200

    Add firmware to check FPGA's IDCODE register

---
+++ firmware/identify_fpga/jtag_tap.c
@@ -0,0 +1,34 @@
+// Copyright (C) 2025 Hansem Ro <hansemro@outlook.com>
+// SPDX-License-Identifier: 0BSD
+
+#include <fx2regs.h>
+#include <fx2delay.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "jtag_tap.h"
+
+void tap_send_tms(uint32_t tms, int n) {
+    for (int i = 0; i < n; i++) {
+        TCK_PIN = 0;
+        TMS_PIN = tms & 1;
+        tms >>= 1;
+        delay_us(JTAG_TCK_PERIOD_US);
+        TCK_PIN = 1;
+        delay_us(JTAG_TCK_PERIOD_US);
+    }
+}
+
+uint32_t tap_shift_bits(uint32_t tdi, int n, bool last_tms) {
+    uint32_t tdo = 0;
+    for (int i = 0; i < n; i++) {
+        TCK_PIN = 0;
+        TDI_PIN = tdi & 1;
+        TMS_PIN = last_tms && (i == n - 1);
+        tdi >>= 1;
+        delay_us(JTAG_TCK_PERIOD_US);
+        TCK_PIN = 1;
+        delay_us(JTAG_TCK_PERIOD_US);
+        tdo |= ((uint32_t)TDO_PIN) << i;
+    }
+    return tdo;
+}
